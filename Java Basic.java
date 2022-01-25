9ff9cee916e1756e924a33402850e4ae7a6fb846
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-01-26 03:10:20 +0600

    Add files via upload

---
+++ Java Basic.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Hello world!\n");
+        System.out.print("this is my first java program\t");
+        System.out.print("How are you?\t");
+        System.out.println("I am Nirob.");
+        System.out.println("1813010");
+    }
+}
+//
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int var = 3000;
+
+        System.out.println("value of var = " + var);
+    }
+}
+//
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       double var = 3000.58;
+
+        System.out.println("value of var = " + (var * 1000));
+    }
+}
+//
