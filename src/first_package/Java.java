1eeb81d488e143ebd9d6b341f65b613c45f0542c
Nirob <shawonnirob16@gmail.com>
2022-01-26 02:37:18 +0600

    first opp

---
+++ src/first_package/Java.java
@@ -0,0 +1,10 @@
+package first_package;
+
+public class Java {
+    public static void main(String[] args) {
+        myClass objectVariable = new myClass();
+        objectVariable.myFunction();
+        objectVariable.myFunction2();
+
+    }
+}
---
+++ src/first_package/myClass.java
@@ -0,0 +1,11 @@
+package first_package;
+
+public class myClass {
+    public void myFunction(){
+        System.out.println("Handle myfunction");
+    }
+    public void myFunction2(){
+        System.out.println("Handle myFunction2");
+    }
+
+}
