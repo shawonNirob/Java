64168b740f48306986993ba9693928bd5026cd62
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-01-29 02:26:24 +0600

    Update 1 Syntax.java

--- 1 Syntax.java
+++ 1 Syntax.java
@@ -247,4 +247,18 @@ public class Syntax {
     }
 
 }
+  //
+  public class Student {
+    private int id;
+    private String name;
+    private int age;
+
+    public Student(int studentId, String studentName,int studentAge){
+        id = studentId;
+        name = studentName;
+        age = studentAge;
+
+    }
 //
+      
+      
