f595641fbabcfb960fb62ad729653eeb5849209f
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-02-03 10:10:16 +0600

    Add files via upload

---
+++ Java oop/3. Data Type.java
@@ -0,0 +1,74 @@
+//String Catination
+package binary_search;
+
+public class sqrt {
+    public static void main(String[] args) {
+        System.out.println(3+3*2+"Abcd");
+        System.out.println("Abcd"+3+3*2);
+    }
+}
+//
+package binary_search;
+
+public class sqrt {
+    public static void main(String[] args) {
+        System.out.println((3+3*2)+"Abcd");
+        System.out.println("Abcd"+(3+3*2));
+    }
+}
+//Type Conversion
+package binary_search;
+
+public class sqrt {
+    public static void main(String[] args) {
+        int anInt = 100;
+        long aLong = anInt;
+        float aFloat = aLong;
+
+        System.out.println("Int value "+ anInt);
+        System.out.println("Long value "+ aLong);
+        System.out.println("Float value " + aFloat);
+    }
+}
+//Type Casting
+package binary_search;
+
+public class sqrt {
+    public static void main(String[] args) {
+        double aDouble = 97.05;
+        long aLong = (long) aDouble;
+        int aInt = (int) aLong;
+
+        System.out.println("Double value "+ aDouble);
+        System.out.println("Long value "+ aLong);
+        System.out.println("Integer value " + aInt);
+    }
+}
+//Atter Narrowing Primitive Conversion
+//some data canbe Close
+package binary_search;
+
+public class sqrt {
+    public static void main(String[] args) {
+        double aDouble = 9_788_888_887_7775L;
+        long aLong = (long) aDouble;
+        int aInt = (int) aLong;
+
+        System.out.println("Double value "+ aDouble);
+        System.out.println("Long value "+ aLong);
+        System.out.println("Integer value " + aInt);
+    }
+}
+//
+package binary_search;
+
+public class sqrt {
+    public static void main(String[] args) {
+        int x=4,y=6;
+        int result1, result2 ;
+        result1= x+y/2;
+        result2=(x+y)/2;
+        System.out.println(" "+result1 + " "+result2);
+    }
+}
+//
