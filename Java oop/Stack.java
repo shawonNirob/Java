5bf4df12c45e9c13b687d72526c24c0874d8e702
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-02-25 21:20:09 +0600

    Update Stack.java

--- Java oop/Stack.java
+++ Java oop/Stack.java
@@ -17,3 +17,73 @@ public class StackLeetCode {
         System.out.println(language);
     }
 }
+//
+package dataStructure.learnStack;
+
+import java.util.Stack;
+
+public class StackLeetCode {
+    public static void main(String[] args) {
+        Stack<Character> word = new Stack<>();
+
+        word.add('T');
+        word.add('S');
+        word.add('F');
+
+        System.out.println(word.pop());
+        System.out.println(word.peek());
+        System.out.println(word.push('H'));
+
+        System.out.println(word);
+
+
+    }
+}
+//
+package dataStructure.learnStack;
+
+import java.util.Stack;
+
+public class StackLeetCode {
+    public static void main(String[] args) {
+        Stack<Integer> word = new Stack<>();
+
+        word.add(1);
+        word.add(2);
+        word.add(3);
+
+        System.out.println(word.pop());
+        System.out.println(word.peek());
+        System.out.println(word.push(4));
+
+        System.out.println(word);
+
+
+    }
+}
+//
+package dataStructure.learnStack;
+
+import java.util.Stack;
+
+public class StackLeetCode {
+    public static void main(String[] args) {
+        Stack<Integer> word = new Stack<>();
+
+        word.add(1);
+        word.add(2);
+        word.add(3);
+
+        System.out.println(word.pop());
+        System.out.println(word.peek());
+        System.out.println(word.push(4));
+
+        System.out.println(word.empty());
+
+        word.set(2,10);
+        System.out.println(word.get(2));
+        System.out.println(word.get(0));
+
+    }
+}
+//
