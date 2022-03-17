c2faa59b0ef80be7e30a148ce24e0673bd042462
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-03-17 16:57:36 +0600

    Create LinkedList.java

---
+++ Java oop/LinkedList.java
@@ -0,0 +1,14 @@
+package dataStructure.linkedList;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList<Integer> l = new LinkedList<>();
+        l.add(65); l.add(85); l.add(100);
+        l.add(0,60);
+        System.out.println(l);
+        l.remove(3);
+        System.out.println(l);
+    }
+}
