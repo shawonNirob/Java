df098145b24e83dfaa175251e92128cfedc081b2
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-03-19 01:43:15 +0600

    Update LinkedList.java

--- Java oop/LinkedList.java
+++ Java oop/LinkedList.java
@@ -12,3 +12,20 @@ public class LinkedListDemo {
         System.out.println(l);
     }
 }
+//
+package dataStructure.linkedList;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList<Integer> l = new LinkedList<>();
+        l.add(65); l.add(85); l.add(100);
+        l.add(0,60);
+        l.addFirst(200);
+        System.out.println(l);
+        l.remove(3);
+        System.out.println(l);
+    }
+}
+//
