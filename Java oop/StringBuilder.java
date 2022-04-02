1dd8f5758dec740444473341126cac52d824a388
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-04-02 12:53:23 +0600

    Update StringBuilder.java

--- Java oop/StringBuilder.java
+++ Java oop/StringBuilder.java
@@ -11,3 +11,21 @@ public class StringBuilderDemo {
         System.out.println(test);
     }
 }
+//StringBuilder have a reverse method;
+package learnJavaClass;
+import java.lang.StringBuilder;
+
+public class StringBuilderDemo {
+    public static void main(String[] args) {
+        StringBuilder test = new StringBuilder("test case: ");
+        test.append("case no 01");
+        test.replace(0,4, "Test");
+        System.out.println(test);
+
+        test.delete(19,21);
+        System.out.println(test);
+
+        test.reverse();
+        System.out.println(test);
+    }
+}
