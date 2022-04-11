b6d253d82abba6e421c735aa4edd90f0fba4bcda
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-04-11 19:03:55 +0600

    Update HashMap.java

--- Java oop/HashMap.java
+++ Java oop/HashMap.java
@@ -76,3 +76,40 @@ public class learnHashMap {
     }
 }
 //
+package leetCode;
+
+import collectionFramework.Collection;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+public class BaseBall {
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("a", 100);
+        map.put("b", 200);
+        map.put("c", 300);
+        map.put("d", 400);
+
+
+        System.out.println("HashMap: "
+                + map.toString());
+
+        // provide key whose value has to be obtained
+        // and default value for the key. Store the
+        // return value in k
+        int k = map.getOrDefault("b", 500);
+
+        map.put("b", map.getOrDefault("b", 0)+50);
+
+        // print the value of k returned by
+        // getOrDefault(Object key, V defaultValue) method
+        System.out.println("Returned Value: " + k);
+
+        System.out.println("HashMap: " + map);
+    }
+}
+//
