d774c4e7d2e8d0853078154cc3b231a6f94e3f46
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-06-20 19:21:59 +0600

    Create static Class.java

---
+++ Java oop/static Class.java
@@ -0,0 +1,19 @@
+package learnJavaClass;
+
+public class StaticClass {
+    private int a=9;
+    private static int b=10;
+    public static void disp(){
+        System.out.println(b);
+    }
+    public void show(){
+        System.out.println(a+" "+ b);
+    }
+
+    public static void main(String[] args) {
+        StaticClass.disp();
+
+        StaticClass nonStatic = new StaticClass();
+        nonStatic.show();
+    }
+}
