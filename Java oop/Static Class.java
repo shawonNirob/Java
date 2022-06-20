3991cf0c07b8e3879c977449362c04758b3919b9
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-06-21 02:57:27 +0600

    Create Static Class.java

---
+++ Java oop/Static Class.java
@@ -0,0 +1,18 @@
+package learnJavaClass;
+
+public class StaticClass {
+
+    static class USB{
+        int usb2 = 2;
+        int usb3 = 1;
+
+        int getTotalPorts(){
+            return usb2 + usb3;
+        }
+    }
+
+    public static void main(String[] args) {
+        StaticClass.USB usb = new StaticClass.USB();
+        System.out.println("Total Ports = " + usb.getTotalPorts());
+    }
+}
