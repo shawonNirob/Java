60dd1bd5b349c1819551f629a1dd872e51322f12
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-06-21 03:00:56 +0600

    Update Static Class.java

--- Java oop/Static Class.java
+++ Java oop/Static Class.java
@@ -16,3 +16,25 @@ public class StaticClass {
         System.out.println("Total Ports = " + usb.getTotalPorts());
     }
 }
+
+//
+
+package learnJavaClass;
+
+public class StaticClass {
+
+    static class USB{
+        int usb2 = 2;
+        int usb3 = 1;
+
+        int getTotalPorts(){
+            return usb2 + usb3;
+        }
+    }
+
+    public static void main(String[] args) {
+        USB usb = new USB();
+        System.out.println("Total Ports = " + usb.getTotalPorts());
+    }
+}
+
