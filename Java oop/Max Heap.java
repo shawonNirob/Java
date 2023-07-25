44943a72c0a900221bb6af171d4e05c12a885a46
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2023-07-25 21:16:01 +0600

    Update Max Heap.java

--- Java oop/Max Heap.java
+++ Java oop/Max Heap.java
@@ -20,3 +20,36 @@ public class MaxHeap {
         System.out.println(pQueue);
     }
 }
+
+//
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        PriorityQueue<Edges> maxHeap = new PriorityQueue<>(Comparator.comparingInt((Edges edges) -> edges.weight).reversed());
+
+        // Adding some sample edges to the max-heap
+        maxHeap.add(new Edges(1, 2, 10));
+        maxHeap.add(new Edges(2, 3, 5));
+        maxHeap.add(new Edges(3, 4, 7));
+
+        // Dequeuing elements in descending order of weight (max-heap behavior)
+        while (!maxHeap.isEmpty()) {
+            Edges edge = maxHeap.poll();
+            System.out.println("Edge: " + edge.source + " -> " + edge.destination + ", Weight: " + edge.weight);
+        }
+    }
+}
+
+class Edges {
+    int source;
+    int destination;
+    int weight;
+
+    public Edges(int source, int destination, int weight) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
+}
+//
